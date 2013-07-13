160f19569006805d71d36bad74c3373630a9c21c
lazerhacker <lazerhacker@rlazerowitz-mbp15-1.local>
2013-07-13 01:31:42 -0700

    Fresh

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hexis::Application.config.secret_token = '141cb912c5dbd67091c0272045f1f5e3a0abf19537277aa869f6538d31ba00fd0f58758564a2882d4285fb40338d8a09955d4f8fefb5370cff96d1908703f753'
